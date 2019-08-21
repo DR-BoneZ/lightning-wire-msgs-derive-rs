574419ca12d44feb9918329c0ca161c178c4660a
Aiden McClelland <me@drbonez.dev>
2019-08-21 16:27:21 -0600

    remove dyn

--- src/lib.rs
+++ src/lib.rs
@@ -3,6 +3,7 @@ extern crate proc_macro;
 use crate::proc_macro::TokenStream;
 use proc_macro2::Span;
 use quote::quote;
+use std::collections::HashSet;
 use syn;
 
 #[proc_macro_derive(WireMessage, attributes(msg_type, tlv_type))]
@@ -36,100 +37,180 @@ fn impl_wire_message(ast: &syn::DeriveInput) -> TokenStream {
         .next()
         .expect("missing attribute \"msg_type\"\n\nhelp: add #[msg_type = ...]");
     let iter = syn::Ident::new(&format!("{}Iter", name), Span::call_site());
+    let item = syn::Ident::new(&format!("{}Items", name), Span::call_site());
     let counter = std::iter::successors(Some(0), |a| Some(a + 1))
         .map(|i| proc_macro2::Literal::usize_suffixed(i));
     let mut tlv = None;
-    let field_mapper = |(i, f): (usize, &syn::Field)| -> (syn::Member, Option<syn::Lit>) {
-        let mut new_tlv = None;
-        let res = (
-            f.ident
-                .as_ref()
-                .map(|id| syn::Member::Named(id.clone()))
-                .unwrap_or_else(|| {
-                    syn::Member::Unnamed(syn::Index {
-                        index: i as u32,
-                        span: Span::call_site(),
-                    })
-                }),
-            f.attrs
-                .iter()
-                .filter_map(|a| match a.parse_meta() {
-                    Ok(m) => match m {
-                        syn::Meta::NameValue(nv) => {
-                            if nv.path.is_ident("tlv_type") {
-                                if let syn::Lit::Int(ref lit) = nv.lit {
-                                    new_tlv = Some(
-                                        lit.base10_parse::<u64>().expect("tlv_type must be a u64"),
-                                    );
-                                    Some(nv.lit)
+    let field_mapper =
+        |(i, f): (usize, &syn::Field)| -> ((syn::Member, syn::Type), Option<syn::Lit>) {
+            let mut new_tlv = None;
+            let mut res = (
+                (
+                    f.ident
+                        .as_ref()
+                        .map(|id| syn::Member::Named(id.clone()))
+                        .unwrap_or_else(|| {
+                            syn::Member::Unnamed(syn::Index {
+                                index: i as u32,
+                                span: Span::call_site(),
+                            })
+                        }),
+                    f.ty.clone(),
+                ),
+                f.attrs
+                    .iter()
+                    .filter_map(|a| match a.parse_meta() {
+                        Ok(m) => match m {
+                            syn::Meta::NameValue(nv) => {
+                                if nv.path.is_ident("tlv_type") {
+                                    if let syn::Lit::Int(ref lit) = nv.lit {
+                                        new_tlv = Some(
+                                            lit.base10_parse::<u64>()
+                                                .expect("tlv_type must be a u64"),
+                                        );
+                                        Some(nv.lit)
+                                    } else {
+                                        panic!("tlv_type must be a u64")
+                                    }
                                 } else {
-                                    panic!("tlv_type must be a u64")
+                                    None
                                 }
-                            } else {
-                                None
                             }
-                        }
-                        _ => None,
-                    },
-                    Err(_) => None,
-                })
-                .next(),
-        );
-        match (tlv, new_tlv) {
-            (Some(_), None) => panic!("tlv stream must occur after expected fields"),
-            (Some(old), Some(new)) if old > new => {
-                panic!("tlv stream must be monotonically increasing by type")
-            }
-            _ => (),
+                            _ => None,
+                        },
+                        Err(_) => None,
+                    })
+                    .next(),
+            );
+            match (tlv, new_tlv) {
+                (Some(_), None) => panic!("tlv stream must occur after expected fields"),
+                (Some(old), Some(new)) if old > new => {
+                    panic!("tlv stream must be monotonically increasing by type")
+                }
+                (_, Some(_)) => match &f.ty {
+                    syn::Type::Path(ref p)
+                        if p.path.segments.last().expect("missing type").ident == "Option" =>
+                    {
+                        (res.0).1 = match &p.path.segments.last().unwrap().arguments {
+                            syn::PathArguments::AngleBracketed(a) => {
+                                (match a.args.first().expect("tlv value must be Option") {
+                                    syn::GenericArgument::Type(t) => t.clone(),
+                                    _ => panic!("tlv value must be Option"),
+                                })
+                            }
+                            _ => panic!("tlv value must be Option"),
+                        };
+                    }
+                    _ => panic!("tlv value must be Option"),
+                },
+                (_, None) => match &f.ty {
+                    syn::Type::Path(ref p)
+                        if p.path.segments.last().expect("missing type").ident == "Option" =>
+                    {
+                        panic!("Option fields must be tlv")
+                    }
+                    _ => (),
+                },
+            };
+            tlv = new_tlv;
+            res
         };
-        tlv = new_tlv;
-        res
-    };
     let punc = syn::punctuated::Punctuated::<syn::Field, ()>::new();
-    let (field, tlv_type): (Vec<syn::Member>, Vec<Option<syn::Lit>>) = match &ast.data {
-        syn::Data::Struct(d) => match &d.fields {
-            syn::Fields::Named(n) => n.named.iter(),
-            syn::Fields::Unnamed(n) => n.unnamed.iter(),
-            syn::Fields::Unit => punc.iter(),
-        },
-        _ => unimplemented!(),
-    }
-    .enumerate()
-    .map(field_mapper)
-    .unzip();
-    let gen = quote! {
-        pub struct #iter<'a> {
-            idx: usize,
-            parent: &'a #name,
+    let (field_tup, tlv_type): (Vec<(syn::Member, syn::Type)>, Vec<Option<syn::Lit>>) =
+        match &ast.data {
+            syn::Data::Struct(d) => match &d.fields {
+                syn::Fields::Named(n) => n.named.iter(),
+                syn::Fields::Unnamed(n) => n.unnamed.iter(),
+                syn::Fields::Unit => punc.iter(),
+            },
+            _ => unimplemented!(),
         }
-        impl<'a> Iterator for #iter<'a> {
-            type Item = EncodedItem<&'a dyn WireItemBoxedWriter>;
-
-            fn next(&mut self) -> Option<Self::Item> {
-                let n = self.idx;
-                self.idx += 1;
-                match n {
-                    #(
-                        #counter => Some((&self.parent.#field, #tlv_type).into()),
-                    )*
-                    _ => None
+        .enumerate()
+        .map(field_mapper)
+        .unzip();
+    let (field, field_ty_set): (Vec<syn::Member>, HashSet<syn::Type>) =
+        field_tup.into_iter().unzip();
+    let field_ty = field_ty_set.iter();
+    let field_ty2 = field_ty_set.iter();
+    let field_ty_name =
+        (0..(field_ty_set.len())).map(|i| syn::Ident::new(&format!("T{}", i), Span::call_site()));
+    let field_ty_name2 = field_ty_name.clone();
+    let field_ty_name3 = field_ty_name.clone();
+    let gen = if field.is_empty() {
+        quote! {
+            impl<'a> IntoIterator for &'a #name {
+                type Item = !;
+                type IntoIter = std::iter::Empty;
+                fn into_iter(self) -> std::iter::Empty<!> {
+                    std::iter::empty()
+                }
+            }
+            impl WireMessage<'_> for #name {
+                fn msg_type(&self) -> u16 {
+                    #num
                 }
             }
         }
-
-        impl<'a> IntoIterator for &'a #name {
-            type Item = <#iter<'a> as Iterator>::Item;
-            type IntoIter = #iter<'a>;
-            fn into_iter(self) -> #iter<'a> {
-                #iter {
-                    idx: 0,
-                    parent: self,
+    } else {
+        quote! {
+            pub enum #item<'a> {
+                #(
+                    #field_ty_name(&'a #field_ty),
+                )*
+            }
+            #(
+                impl<'a> From<&'a #field_ty2> for #item<'a> {
+                    fn from(t: &'a #field_ty2) -> Self {
+                        #item::#field_ty_name2(t)
+                    }
+                }
+            )*
+            impl<'a> WireItem for #item<'a> {
+                fn encode<W: std::io::Write>(&self, w: &mut W) -> std::io::Result<usize> {
+                    use #item::*;
+                    match self {
+                        #(
+                            #field_ty_name3(a) => a.encode(w),
+                        )*
+                    }
                 }
             }
-        }
-        impl WireMessage<'_> for #name {
-            fn msg_type(&self) -> u16 {
-                #num
+
+            pub struct #iter<'a> {
+                idx: usize,
+                parent: &'a #name,
+            }
+            impl<'a> Iterator for #iter<'a> {
+                type Item = EncodedItem<#item<'a>>;
+
+                fn next(&mut self) -> Option<Self::Item> {
+                    let n = self.idx;
+                    self.idx += 1;
+                    match n {
+                        #(
+                            #counter => Some(EncodedItem::from((&self.parent.#field, #tlv_type))),
+                        )*
+                        _ => None
+                    }
+                }
+            }
+
+            impl<'a> IntoIterator for &'a #name {
+                type Item = <#iter<'a> as Iterator>::Item;
+                type IntoIter = #iter<'a>;
+                fn into_iter(self) -> #iter<'a> {
+                    #iter {
+                        idx: 0,
+                        parent: self,
+                    }
+                }
+            }
+            impl<'a> WireMessage<'a> for #name {
+                type Item = #item<'a>;
+
+                fn msg_type(&self) -> u16 {
+                    #num
+                }
             }
         }
     };
